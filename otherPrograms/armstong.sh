65c32c0cff0caefcc6172790a96c6c598e810419
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2020-11-28 14:22:46 +0530

    Create armstong.sh

---
+++ otherPrograms/armstong.sh
@@ -0,0 +1,26 @@
+# check a number is armstrong or not
+echo "Enter the number"
+read n
+function ams
+{
+t=$n
+s=0
+b=0
+c=10
+while [ $n -gt $b ]
+do
+r=$((n % c))
+i=$((r * r * r))
+s=$((s + i))
+n=$((n / c)) # u cant do n/10
+done
+echo $s
+if [ $s == $t ]
+then
+echo "Amstrong number"
+else
+echo "Not an Armstrong number"
+fi
+}
+result=`ams $n`
+echo "$result"
