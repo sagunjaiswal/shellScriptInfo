e72252a0f5d2ca9d7b5a857291d36ec8627c8903
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-02-23 17:31:03 +0530

    Update program1.sh

--- arrays/program1.sh
+++ arrays/program1.sh
@@ -4,6 +4,7 @@ arr[2]="Second"
 arr[3]="Third"
 arr[4]="Four"
 
+#different ways to print the array
 echo "First Method: ${arr[*]}"
 echo "Second Method: ${arr[@]}"
 echo "Third Method : ${arr[@]:0}"     
