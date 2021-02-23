62c88557b04ba76ad0da9a909ae10ecf3284e9ee
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-02-23 18:28:56 +0530

    Update program2.sh

--- arrays/program2.sh
+++ arrays/program2.sh
@@ -1,2 +1,14 @@
+#way to initialize an array
 arr=(Zero One Two Three Four 5 Six)
 echo "${arr[*]}"
+
+#to change any element at the given position
+arr[2]="changed"
+echo "${arr[*]}"
+#prints Zero One Changed Three Four 5 Six
+
+#another way to change an element at a given position
+newVal=2
+arr[2]=$newVal
+echo "${arr[*]}"
+#prints Zero One 2 Three Four 5 Six
